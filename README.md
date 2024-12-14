a7b747cbc61f227307837cd63259e24d32b42228
LaylaFe <laylafe82@gmail.com>
2024-12-14 16:26:56 -0300

    Correção do arquivo Readme

---
+++ README.md
@@ -0,0 +1,64 @@
+# Frontend Mentor - Age calculator app
+
+![Design preview for the Age calculator app coding challenge](./design/desktop-preview.jpg)
+
+# Solução do desafio de criar uma calculadora de idade no [Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q). 
+
+## 📖 Ìndice
+
+- [Meu processo](#meu-processo)
+- [Recursos utilizados](#Recursos-utilizados)
+- [Aprendizado](#Aprendizado)
+- [Imagem Demonstração](#imagem_demosntracao)
+- [Autor](#Autor)
+
+# ⭐ Meu processo
+
+- Utilizando o VS Code conectado a minha conta do GitHub, desenvolvi uma página em HTML e CSS, responsivo, para a visualização e utilização do desafio de Criar uma Calculadora de Idade no site [Frontend Mentor](https://www.frontendmentor.io/challenges/age-calculator-app-dF9DFFpj-Q) e concluir o desafio de codificação criado.
+
+# ⭐ Recursos utilizados
+
+![Git](https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white)![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![Visual Studio Code](https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)
+
+- Visual Studio Code
+- Marcação semântica HTML5
+- Propriedades personalizadas do CSS
+- Modelos de caixa
+- Flexbox
+- CSS Grid
+- Fluxo de trabalho que prioriza dispositivos móveis
+- [Markdown Badges](https://github.com/Ileriayo/markdown-badges#markdown-badges)
+- [Como escrever no GitHub](https://docs.github.com/pt/get-started/writing-on-github)
+
+# ⭐ Aprendizado
+
+Aprendi a adicionar códigos de JavaScript para calcular a idade em dias, meses e anos.
+```
+        span[0].innerHTML = novoAno;
+        span[1].innerHTML = novoMes;
+        span[2].innerHTML = novoDia;
+```
+```
+  const calculoIdade = () => {}
+```
+
+# 📌 Imagem Demonstração
+
+![](design/mobile-design.jpg)
+
+
+## 🌙⭐ Autor
+[![LinkedIn](https://img.shields.io/badge/-laylafe-000?style=for-the-badge&logo=linkedin&logoColor=62b1d4&color:FFF)](https://www.linkedin.com/in/laylafe/)
+[![github](https://img.shields.io/badge/-laylafe-000?style=for-the-badge&logo=github&)](https://github.com/laylafe)
+- Website - [Missão de Amor](https://laylafe.github.io/missaodeamor/) - Site em desenvolvimento
+- Frontend Mentor - [@LaylaFe](https://www.frontendmentor.io/profile/LaylaFe)
+
+_____________________________
+
+![Top Langs](https://github-readme-stats-git-masterrstaa-rickstaa.vercel.app/api/top-langs/?username=laylafe&layout=compact&bg_color=211452&border_color=00000000&title_color=E94D5F&text_color=FFF)
+
+## GitHub Stats
+
+![GitHub Stats](https://github-readme-stats.vercel.app/api?username=laylafe&theme=transparent&bg_color=211452&border_color=fff0&show_icons=true&icon_color=fff&title_color=fff&text_color=fff&hide_title=true&hide=stars)
---
+++ script.js
@@ -0,0 +1,111 @@
+window.onload = function CalcularIdade() {
+    var dia = document.getElementById('dia');
+    var mes = document.getElementById('mes');
+    var ano = document.getElementById('ano');
+    var label = document.getElementsByTagName('label');
+    var erro = document.getElementsByClassName('erro');
+    var submit = document.getElementById('submit');
+    var span = document.getElementsByTagName('span');
+
+    var novaData = new Date();
+    
+    var idadeAnoAtual = novaData.getFullYear();
+    var idadeMesAtual = novaData.getMonth() + 1;
+    var idadeDiaAtual = novaData.getDate();
+
+    const tipodoErro = [
+        "",
+        "Digite uma data válida.",
+        "Coloque um mês válido.",
+        "Coloque um ano válido.",
+        "Coloque um dia válido."
+    ];
+
+    const estadodoErro = (numerodoErro, tipodoErro, tipodeData, color) => {
+        erro[numerodoErro].innerHTML = tipodoErro;
+        label[numerodoErro].style.color = color;
+        tipodeData.style.borderColor = color;
+    }
+
+    const anoBissexto = (dia, mes, ano) => {
+        mes = mes - 1;
+        var dataCompleta = new Date(ano, mes, dia);
+        return (dia == dataCompleta.getDate() && mes == dataCompleta.getMonth() && ano == dataCompleta.getFullYear());
+    }
+
+    const calculoIdade = () => {
+        var novoAno = Math.abs(idadeAnoAtual - ano.value);
+        var novoMes = 0;
+        if (idadeMesAtual >= mes.value) {
+            novoMes = idadeMesAtual - mes.value;
+        } else {
+            novoAno--;
+            novoMes = 12 + idadeMesAtual - mes.value;
+        }
+        var novoDia = 0;
+        if (idadeDiaAtual >= dia.value) {
+            novoDia = idadeDiaAtual - dia.value;
+        } else {
+            novoMes--;
+            novoDia = 30 + idadeDiaAtual - dia.value;
+            if (novoMes < 0) {
+                novoMes = 11;
+                novoAno--;
+            }
+        }
+        span[0].innerHTML = novoAno;
+        span[1].innerHTML = novoMes;
+        span[2].innerHTML = novoDia; 
+    }
+
+    const verificaDia = () => {
+        if (dia.value == "") {
+            estadodoErro(0, tipodoErro[1], dia, "#ff5757");
+            return false;
+        } else if (dia.value <= 0 || dia.value > 31) {
+            estadodoErro(0, tipodoErro[2], dia, "#ff5757");
+            return false;
+        } else if (!anoBissexto(dia.value, mes.value, ano.value)) {
+            estadodoErro(0, tipodoErro[4], dia, "#ff5757");
+            return false;
+        } else {
+            estadodoErro(0, tipodoErro[0], dia, "");
+            return true;
+        }
+    }
+
+    const verificaMes = () => {
+        if (mes.value == "") {
+            estadodoErro(1, tipodoErro[1], mes, "#ff5757");
+            return false;
+        } else if (mes.value <= 0 || mes.value > 12) {
+            estadodoErro(1, tipodoErro[3], mes, "#ff5757");
+            return false;
+        } else {
+            estadodoErro(1, tipodoErro[0], mes, "");
+            return true;
+        }
+    }
+
+    const verificaAno = () => {
+        if (ano.value == "") {
+            estadodoErro(2, tipodoErro[1], ano, "#ff5757");
+            return false;
+        } else if (ano.value > idadeAnoAtual) {
+            estadodoErro(2, tipodoErro[4], ano, "#ff5757");
+            return false;
+        } else {
+            estadodoErro(2, tipodoErro[0], ano, "");
+            return true;
+        }
+    }
+   submit.addEventListener("click", () => {
+        let diaValido = verificaDia();
+        let mesValido = verificaMes();
+        let anoValido = verificaAno();
+
+        if (diaValido && mesValido && anoValido) {
+            calculoIdade();
+        }
+    });
+}
